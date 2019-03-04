70032fad8dfeaa4aa2d6ee4e4598961697bf308c
Bernardo Lins <bernardolins28@gmail.com>
2019-03-04 20:36:16 -0300

    feat(cli): add basic options to cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
+const VERSION: &str = "0.0.1";
+const APP_NAME: &str = "cr7";
+
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new(APP_NAME)
+                .version(VERSION)
+                .about("Container runtime")
+                .arg(
+                    Arg::with_name("version")
+                        .short("v")
+                )
+                .subcommand(
+                    SubCommand::with_name("init")
+                        .about("Creates a new specification file")
+                )
+                .subcommand(
+                    SubCommand::with_name("create")
+                        .about("Creates a new container using the specification")
+                )
+                .subcommand(
+                    SubCommand::with_name("delete")
+                        .about("Delestes a running container")
+                );
+
+    let _matches = app.get_matches();
 }
