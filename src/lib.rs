990cf46e23e3d89ede7273cf038e0e68077af8d1
Bernardo Lins <bernardolins28@gmail.com>
2019-03-05 00:07:29 -0300

    feat(spec): set default values for oci_version and hostname

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,10 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Spec {
+    #[serde(default = "Spec::default_oci_version")]
     pub oci_version: String,
+
+    #[serde(default = "Spec::default_hostname")]
     pub hostname: String,
 }
 
@@ -16,9 +19,66 @@ impl Spec {
     pub fn from_json(path: &str) -> Result<Spec, Box<Error>> {
         let file = File::open(path)?;
         let reader = BufReader::new(file);
-
         let spec = serde_json::from_reader(reader)?;
 
         Ok(spec)
+    }
+
+    fn default_oci_version() -> String {
+        "1.0.1-dev".to_string()
+    }
+
+    fn default_hostname() -> String {
+        "cr7".to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+    use std::fs::File;
+    use std::io::Write;
+
+
+    #[test]
+    fn from_json_creates_a_spec_from_json() {
+        let contents = b"\
+{
+    \"ociVersion\": \"1.0.0\",
+    \"hostname\": \"container-hostname\"
+}";
+        let mut dir = env::temp_dir();
+        dir.push("from_json.json");
+
+        let mut file = File::create(&dir).unwrap();
+
+        let path = dir.to_str().unwrap();
+
+        file.write_all(contents).unwrap();
+
+        let spec = Spec::from_json(path).unwrap();
+        assert_eq!(spec.oci_version, "1.0.0");
+        assert_eq!(spec.hostname, "container-hostname");
+
+        std::fs::remove_file(path).unwrap();
+    }
+
+    #[test]
+    fn from_json_creates_a_spec_with_default_oci_version() {
+        let mut dir = env::temp_dir();
+        dir.push("defaults.json");
+
+        let mut file = File::create(&dir).unwrap();
+        let path = dir.to_str().unwrap();
+
+        file.write_all(b"{}").unwrap();
+
+        let spec = Spec::from_json(dir.to_str().unwrap()).unwrap();
+
+        assert_eq!(spec.oci_version, Spec::default_oci_version());
+        assert_eq!(spec.hostname, Spec::default_hostname());
+
+        std::fs::remove_file(path).unwrap();
     }
 }
