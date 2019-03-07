a41eff4244c5e12d1645129bdaa9e4ccd55f5c54
Bernardo Lins <bernardolins28@gmail.com>
2019-03-07 08:33:37 -0300

    chore(cli): using clap yaml to parse cli

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,13 @@
+#[macro_use]
 extern crate clap;
 extern crate cr7;
 
-use clap::{Arg, App, SubCommand};
-
 use cr7::oci;
-
-const VERSION: &str = "0.0.1";
-const APP_NAME: &str = "cr7";
+use clap::App;
 
 fn main() {
-    let app = App::new(APP_NAME)
-                .version(VERSION)
-                .about("Container runtime")
-                .arg(
-                    Arg::with_name("version")
-                        .short("v")
-                )
-                .subcommand(
-                    SubCommand::with_name("init")
-                        .about("Creates a new specification file")
-                )
-                .subcommand(
-                    SubCommand::with_name("run")
-                        .about("Creates a new container using the specification")
-                        .arg(Arg::with_name("container-id")
-                            .help("Creates and runs a container")
-                            .required(true)
-                            .index(1)
-                        )
-                        .arg(Arg::with_name("spec")
-                            .help("The spec file path")
-                            .short("s")
-                            .long("spec")
-                            .takes_value(true)
-                        )
-                )
-                .subcommand(
-                    SubCommand::with_name("delete")
-                        .about("Deletes a running container")
-                );
-
-    let matches = app.get_matches();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 
     if let Some(matches) = matches.subcommand_matches("run") {
         let spec_path = matches.value_of("spec").unwrap_or("config.json");
