65de5b23795e7a9b57a8c77910fee99f020a0078
Bernardo Lins <bernardolins28@gmail.com>
2019-03-12 09:08:33 -0300

    fix(lib): fix compilation when platform is not linux

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Error as IOError;
+
+#[cfg(target_os = "linux")]
 use std::error::Error;
 
 pub fn read_config(config_path: &str) -> Result<BufReader<File>, IOError> {
@@ -22,7 +24,7 @@ pub fn build_paltform_spec<R: BufRead>(reader: R) -> Result<linux::spec::Spec, B
 }
 
 #[cfg(not(target_os = "linux"))]
-pub fn build_paltform_spec(reader: BufReader) -> Result<(), Box<Error>> {
+pub fn build_paltform_spec<R: BufRead>(_reader: R) -> Result<(), &'static str> {
     Err("Platform not supported")
 }
 
