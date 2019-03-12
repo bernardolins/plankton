59887199a65fe84cfb91ddda7055458798ae272f
Bernardo Lins <bernardolins28@gmail.com>
2019-03-12 09:15:29 -0300

    feat(config): do not panic when there is an error reading the config file

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,18 @@ fn main() {
 
     if let Some(matches) = matches.subcommand_matches("run") {
         let spec_path = matches.value_of("spec").unwrap_or("config.json");
-        let config = cr7::read_config(spec_path).unwrap();
-        match cr7::build_paltform_spec(config) {
-            Ok(spec) => {
-                println!("{:?}", spec);
-            }
+
+        let config = match cr7::read_config(spec_path) {
+            Ok(config) => config,
             Err(err) => {
                 println!("{}", err);
+                std::process::exit(1);
             }
+        };
+
+        match cr7::build_paltform_spec(config) {
+            Ok(spec) => println!("{:?}", spec),
+            Err(err) => println!("{}", err)
         }
     }
 }
