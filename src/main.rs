3d25d633c61937fec74950c5e7d7513c2bd72025
Bernardo Lins <bernardolins28@gmail.com>
2019-03-14 09:00:47 -0300

    fix(cli): fix wrong flag name on main

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let matches = App::from_yaml(yaml).get_matches();
 
     if let Some(matches) = matches.subcommand_matches("create") {
-        let spec_path = matches.value_of("spec").unwrap_or("config.json");
+        let spec_path = matches.value_of("config").unwrap_or("config.json");
 
         let config = match cr7::read_config(spec_path) {
             Ok(config) => config,
