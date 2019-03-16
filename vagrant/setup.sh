f61b248fec09420970e21f5ede807f1357c7ab71
Bernardo Lins <bernardolins28@gmail.com>
2019-03-16 09:49:00 -0300

    chore(vagrant): improve builld script and vagrant file

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,32 +1,5 @@
 #!/bin/sh
 
-# Git
 sudo dnf install -y git
-
-# Rust
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.0
-echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-
-asdf plugin-add rust
-asdf install rust 1.32.0
-
-# runc
-sudo dnf install -y \
-  btrfs-progs-devel \
-  containers-common \
-  device-mapper-devel \
-  git \
-  glib2-devel \
-  glibc-devel \
-  glibc-static \
-  go \
-  golang-github-cpuguy83-go-md2man \
-  gpgme-devel \
-  libassuan-devel \
-  libgpg-error-devel \
-  libseccomp-devel \
-  libselinux-devel \
-  ostree-devel \
-  pkgconfig \
-  runc
+sudo dnf install -y rust
+sudo dnf install -y cargo
