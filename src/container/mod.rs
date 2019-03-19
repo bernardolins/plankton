efac772e4f991b02cc07f799c90a9e5f9d6d7f32
Bernardo Lins <bernardolins28@gmail.com>
2019-03-19 09:46:09 -0300

    feat(container): add bundle to container struct

--- src/container/mod.rs
+++ src/container/mod.rs
@@ -1,17 +1,26 @@
 mod status;
+mod bundle;
 
 use std::error::Error;
 use self::status::Status;
+use self::bundle::Bundle;
 
 struct Container {
     id: String,
     status: Status,
+    bundle: Bundle,
 }
 
 impl Container {
-    pub fn new(id: &str) -> Result<Container, Box<Error>> {
-        let container = Container {status: Status::Creating};
+    pub fn new(id: &str, bundle_path: &str) -> Result<Container, Box<Error>> {
+        let bundle = Bundle::new(bundle_path)?;
+
+        let container = Container {
             id: id.to_string(),
+            status: Status::Creating,
+            bundle: bundle,
+        };
+
         Ok(container)
     }
 }
@@ -19,10 +28,18 @@ impl Container {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::env;
+    use std::fs;
+
+    fn create_temp_dir(dir_name: &str) -> String {
+        let tmp_dir = env::temp_dir().join(dir_name);
+        fs::create_dir_all(&tmp_dir).unwrap();
+        tmp_dir.to_str().unwrap().to_string()
+    }
 
     #[test]
     fn test_new() {
-        let container = Container::new("test_container").unwrap();
+        let container = Container::new("test_container", &create_temp_dir("oci_bundle")).unwrap();
         assert_eq!(container.id, "test_container");
         assert_eq!(container.status, Status::Creating);
     }
