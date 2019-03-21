3428d7aed79ff9f3960caff15b9612042fbebb66
Bernardo Lins <bernardolins28@gmail.com>
2019-03-21 08:58:19 -0300

    feat(cli): change config path option to bundle path

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,19 @@ extern crate cr7;
 
 use clap::App;
 use cr7::spec;
+use cr7::container;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
     if let Some(matches) = matches.subcommand_matches("create") {
-        let spec_path = matches.value_of("config").unwrap_or("config.json");
+        let bundle_path = matches.value_of("bundle").unwrap_or(".");
+        let container_id = matches.value_of("container-id").unwrap();
 
-        let config = match cr7::read_config(spec_path) {
-            Ok(config) => config,
-            Err(err) => {
-                println!("{}", err);
-                std::process::exit(1);
-            }
-        };
-
-        match spec::build(config) {
-            Ok(spec) => println!("{:?}", spec),
+        let container = match container::Container::new(container_id, bundle_path) {
+            Ok(container) => println!("{:?}", container),
             Err(err) => println!("{}", err)
-        }
+        };
     }
 }
