d88ee95702e0c5f76b9a19ea2e4626ee7a8d27a6
Bernardo Lins <bernardolins28@gmail.com>
2019-03-21 08:57:55 -0300

    feat(container): remove bundle module
    Now the bundle path existence is being validated on container module

--- src/container/mod.rs
+++ src/container/mod.rs
@@ -1,46 +1,68 @@
 mod status;
-mod bundle;
+mod error;
 
-use std::error::Error;
+use container::error::Error;
+use std::path::PathBuf;
+
 use self::status::Status;
-use self::bundle::Bundle;
 
-struct Container {
+const CONFIG_FILE_NAME: &str = "config.json";
+
+#[derive(Debug)]
+pub struct Container {
     id: String,
     status: Status,
-    bundle: Bundle,
+    bundle_path: PathBuf,
+    config_path: PathBuf,
 }
 
 impl Container {
-    pub fn new(id: &str, bundle_path: &str) -> Result<Container, Box<Error>> {
-        let bundle = Bundle::new(bundle_path)?;
+    pub fn new(id: &str, bundle_path: &str) -> Result<Container, Error> {
+        let bundle_path = PathBuf::from(bundle_path);
+        let config_path = bundle_path.join(CONFIG_FILE_NAME);
+
+        let absolute_bundle_path = canonicalize_path(bundle_path)?;
+        let absolute_config_path = canonicalize_path(config_path)?;
 
         let container = Container {
             id: id.to_string(),
             status: Status::Creating,
-            bundle: bundle,
+            bundle_path: absolute_bundle_path,
+            config_path: absolute_config_path,
         };
 
         Ok(container)
     }
 }
 
+fn canonicalize_path(path: PathBuf) -> Result<PathBuf, Error> {
+    match path.canonicalize() {
+        Ok(path) => Ok(path),
+        Err(io_error) => Err(Error::from(io_error))
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use std::env;
-    use std::fs;
+    extern crate tempfile;
 
-    fn create_temp_dir(dir_name: &str) -> String {
-        let tmp_dir = env::temp_dir().join(dir_name);
-        fs::create_dir_all(&tmp_dir).unwrap();
-        tmp_dir.to_str().unwrap().to_string()
-    }
+    use super::*;
+
+    use std::fs::File;
 
     #[test]
-    fn test_new() {
-        let container = Container::new("test_container", &create_temp_dir("oci_bundle")).unwrap();
-        assert_eq!(container.id, "test_container");
-        assert_eq!(container.status, Status::Creating);
+    fn test_bundle_path_must_exist() {
+        let bundle_path = tempfile::tempdir().unwrap();
+        let config_path = bundle_path.path().join(CONFIG_FILE_NAME);
+        let config_file = File::create(config_path).unwrap();
+
+        let ok = Container::new("container1", bundle_path.path().to_str().unwrap());
+        assert!(ok.is_ok(), "expected {:?} to be ok", ok);
+
+        let err = Container::new("container1", "/invalid/path/to/bundle");
+        assert!(err.is_err(), "expected {:?} to be err", err);
+
+        drop(config_file);
+        bundle_path.close().unwrap();
     }
 }
