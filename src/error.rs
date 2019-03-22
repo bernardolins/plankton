a5e70ececb0271e73e3fb81aefe7692419cc4026
Bernardo Lins <bernardolins28@gmail.com>
2019-03-21 22:22:49 -0300

    feat(error): create improved error type

---
+++ src/error.rs
@@ -0,0 +1,43 @@
+pub enum Error {
+    NotFound,
+    PermissionDenied,
+    ConfigSyntax,
+    ParseConfig,
+    ConfigIO,
+    Unknown,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let message = match *self {
+            Error::NotFound => "no such file or directory",
+            Error::PermissionDenied => "permission denied",
+            Error::ConfigSyntax => "config.json is not a valid json",
+            Error::ParseConfig => "invalid data on config.json",
+            Error::ConfigIO => "error opening config.json",
+            Error::Unknown => "unknown error",
+        };
+        write!(f, "{}", message)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(io_error: std::io::Error) -> Error {
+        match io_error.kind() {
+            std::io::ErrorKind::NotFound => Error::NotFound,
+            std::io::ErrorKind::PermissionDenied => Error::PermissionDenied,
+            _ => Error::Unknown,
+        }
+    }
+}
+
+impl From<serde_json::error::Error> for Error {
+    fn from(serde_error: serde_json::error::Error) -> Error {
+        match serde_error.classify() {
+            serde_json::error::Category::Io => Error::ConfigIO,
+            serde_json::error::Category::Syntax => Error::ConfigSyntax,
+            serde_json::error::Category::Data => Error::ParseConfig,
+            _ => Error::Unknown,
+        }
+    }
+}
