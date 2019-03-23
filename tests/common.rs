5248978cc5404597978704aa044c6bf71b31e38c
Bernardo Lins <bernardolins28@gmail.com>
2019-03-23 14:11:51 -0300

    tests(integration): refactor integration tests

--- tests/common.rs
+++ tests/common.rs
@@ -6,23 +6,45 @@ use std::io;
 use std::io::BufReader;
 use tempfile::TempDir;
 
-pub fn dir() -> TempDir {
-    tempfile::tempdir().unwrap()
+pub enum ConfigTemplate {
+    Valid,
+    NoHostname,
 }
 
-pub fn file(name: &str, dir: &TempDir) -> File {
-    let path = PathBuf::from(dir.path());
-    let file_path = path.join(name);
-    File::create(file_path).unwrap()
+impl ConfigTemplate {
+    fn file(&self) -> File {
+        let file_name = match *self {
+            ConfigTemplate::Valid => "valid_config.json",
+            ConfigTemplate::NoHostname => "no_hostname_config.json"
+        };
+
+        let templates_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/templates"));
+        let template_path = templates_path.join(file_name);
+        File::open(template_path).unwrap()
+    }
 }
 
-pub fn read_template(template_name: &str) -> BufReader<File> {
-    let templates_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/templates"));
-    let template_path = templates_path.join(template_name);
-    let template = File::open(template_path).unwrap();
-    BufReader::new(template)
+
+pub struct Bundle {
+    dir: TempDir,
+    path: PathBuf,
 }
 
-pub fn write_to(file: &mut File, contents: &mut BufReader<File>) {
-    io::copy(contents, file).unwrap();
+impl Bundle {
+    pub fn new(template: ConfigTemplate) -> Bundle {
+        let dir = tempfile::tempdir().unwrap();
+        let dir_path = PathBuf::from(dir.path());
+        let config_path = dir_path.join("config.json");
+
+        let mut config_file = File::create(config_path).unwrap();
+        let mut template_content = BufReader::new(template.file());
+        io::copy(&mut template_content, &mut config_file).unwrap();
+
+        Bundle {
+            dir: dir,
+            path: dir_path,
+        }
+    }
+
+    pub fn str_path(&self) -> &str { &self.path.to_str().unwrap() }
 }
--- tests/container.rs
+++ tests/container.rs
@@ -6,26 +6,13 @@ mod common;
 use cr7::container::Container;
 use tempfile::TempDir;
 
-fn setup_bundle(config_template: &str) -> TempDir {
-    let tempdir = common::dir();
-    let mut tempfile = common::file("config.json", &tempdir);
-    let mut template = common::read_template(config_template);
-    common::write_to(&mut tempfile, &mut template);
-    tempdir
-}
+use common::{Bundle, ConfigTemplate};
 
 #[test]
-fn creates_container_from_valid_config() {
-    let bundle_dir = setup_bundle("valid_config.json");
-    let bundle_path = bundle_dir.path().to_str().unwrap();
-    let container = Container::new("some-container", bundle_path);
-    assert!(container.is_ok(), "expect {:?} to be ok", container)
-}
+fn test_create_container() {
+    let bundle = Bundle::new(ConfigTemplate::Valid);
+    let container = Container::new("some-container", bundle.str_path());
 
-#[test]
-fn sets_container_id() {
-    let bundle_dir = setup_bundle("valid_config.json");
-    let bundle_path = bundle_dir.path().to_str().unwrap();
-    let container = Container::new("some-container", bundle_path);
-    assert_eq!(container.unwrap().id(), "some-container")
+    assert!(container.is_ok(), "expect {:?} to be ok", container);
+    assert_eq!(container.unwrap().id(), "some-container");
 }
