e2b3440620ff75bad91e32c1b69b625059a9de0b
Bernardo Lins <bernardolins28@gmail.com>
2019-03-26 08:37:34 -0300

    feat(error): handling errors from nix

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,10 @@ pub enum Error {
     ConfigIO,
     Unknown,
     ContainerAlreadyExists,
+    NoSuchFileOrDirectory,
+    ResourceUnavailable,
+    InvalidArgument,
+    NotEnoughMemory,
 }
 
 impl std::fmt::Display for Error {
@@ -19,6 +23,10 @@ impl std::fmt::Display for Error {
             Error::ConfigIO => "error opening config.json",
             Error::Unknown => "unknown error",
             Error::ContainerAlreadyExists => "container already exists",
+            Error::NoSuchFileOrDirectory => "no such file or directory",
+            Error::ResourceUnavailable => "resource unavailable",
+            Error::InvalidArgument => "invalid argument",
+            Error::NotEnoughMemory => "not enough memory available",
         };
         write!(f, "{}", message)
     }
@@ -41,6 +49,19 @@ impl From<serde_json::error::Error> for Error {
             serde_json::error::Category::Syntax => Error::ConfigSyntax,
             serde_json::error::Category::Data => Error::ParseConfig,
             serde_json::error::Category::Eof => Error::ParseConfig,
+        }
+    }
+}
+
+impl From<nix::Error> for Error {
+    fn from(nix_error: nix::Error) -> Error {
+        match nix_error.as_errno() {
+            Some(nix::errno::Errno::EPERM) => Error::PermissionDenied,
+            Some(nix::errno::Errno::ENOENT) => Error::NoSuchFileOrDirectory,
+            Some(nix::errno::Errno::EAGAIN) => Error::ResourceUnavailable,
+            Some(nix::errno::Errno::EINVAL) => Error::InvalidArgument,
+            Some(nix::errno::Errno::ENOMEM) => Error::NotEnoughMemory,
+            _ => Error::Unknown,
         }
     }
 }
