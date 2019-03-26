7bcfd36f5cdf4d179d92c4e4ccda0a746e77a031
Bernardo Lins <bernardolins28@gmail.com>
2019-03-26 09:26:49 -0300

    refactor(lib): remove unused function on lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,3 @@ pub mod bundle;
 pub mod config;
 pub mod container;
 pub mod error;
-
-use std::fs::File;
-use std::io::BufReader;
-use std::io::Error as IOError;
-
-
-pub fn read_config(config_path: &str) -> Result<BufReader<File>, IOError> {
-    let file = File::open(config_path)?;
-    let reader = BufReader::new(file);
-    Ok(reader)
-}
