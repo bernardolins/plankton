a40b55ed308f4b434b6f4bddfb8358322a39107e
Bernardo Lins <bernardolins28@gmail.com>
2019-04-02 14:18:36 -0300

    feat(cli): using current dir as default for bundle path on run command

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,12 @@ fn main() {
     let matches = App::from_yaml(yaml).get_matches();
 
     /*
-     * create command
+     * run command
      */
-    if let Some(matches) = matches.subcommand_matches("create") {
-        let bundle_path = matches.value_of("bundle").unwrap_or(".");
+    if let Some(matches) = matches.subcommand_matches("run") {
+        let current_dir = str_current_dir();
         let container_id = matches.value_of("container-id").unwrap();
+        let bundle_path = matches.value_of("bundle").unwrap_or(&current_dir);
 
         match container::operations::run(container_id, bundle_path) {
             Err(err) => {
@@ -35,5 +36,20 @@ fn main() {
             Ok(state_json) => println!("{}", state_json),
             Err(err) => println!("{}", err),
         };
+    }
+}
+
+fn str_current_dir() -> String {
+    match std::env::current_dir() {
+        Ok(current_path) => {
+            match current_path.to_str() {
+                Some(dir) => String::from(dir),
+                None => String::from("./")
+            }
+        }
+        Err(err) => {
+            println!("{}", err);
+            std::process::exit(1);
+        }
     }
 }
