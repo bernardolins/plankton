a45db65f46771bb72daca441d83b02c4dd135a3d
Bernardo Lins <bernardolins28@gmail.com>
2019-04-02 22:20:05 -0300

    feat(container): implement cwd on process spec

--- src/config/process.rs
+++ src/config/process.rs
@@ -4,8 +4,10 @@ use serde::{Serialize, Deserialize};
 #[serde(rename_all = "camelCase")]
 pub struct Process {
     args: Vec<String>,
+    cwd: String,
 }
 
 impl Process {
     pub fn args(&self) -> &Vec<String> { &self.args }
+    pub fn cwd(&self) -> &str { &self.cwd }
 }
--- src/container/operations/run.rs
+++ src/container/operations/run.rs
@@ -12,6 +12,9 @@ use nix::sched::CloneFlags;
 use nix::sys::wait;
 
 use std::ffi::CString;
+use std::process;
+use std::path::Path;
+use std::env;
 
 pub fn run(container_id: &str, bundle_path: &str) -> Result<(), Error> {
     let container = environment::load(container_id)?;
@@ -63,6 +66,14 @@ fn clone_callback(process_config: &Process) -> isize {
     let args = process_config.args();
     let cstring_args: Vec<CString> = args.iter().map(|arg| CString::new(arg.to_string()).unwrap()).collect();
     let bin = cstring_args[0].clone();
+
+    let cwd_path = Path::new(process_config.cwd());
+    if !cwd_path.is_absolute() {
+        println!("cwd path {} must be absolute", process_config.cwd());
+        process::exit(1);
+    }
+    env::set_current_dir(&cwd_path).unwrap();
+
 
     unistd::execvp(&bin, &cstring_args[..]).unwrap();
 
