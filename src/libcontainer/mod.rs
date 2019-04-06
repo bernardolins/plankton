7f4f1a3b757213a550741d4609d0aeb74731dcb5
Bernardo Lins <bernardolins28@gmail.com>
2019-04-06 17:37:18 -0300

    feat(libcontainer): expose structures on libcontainer base

--- src/libcontainer/mod.rs
+++ src/libcontainer/mod.rs
@@ -7,3 +7,12 @@ pub use self::linux::environment::Environment;
 
 #[cfg(target_os = "linux")]
 pub use self::linux::container::Container;
+
+#[cfg(target_os = "linux")]
+pub use self::linux::namespace::Namespace;
+
+#[cfg(target_os = "linux")]
+pub use self::linux::namespace::NamespaceList;
+
+#[cfg(target_os = "linux")]
+pub use self::linux::namespace::NamespaceType;
