800be878b56222dee01a0a0d1fd2392f82be5644
Bernardo Lins <bernardolins28@gmail.com>
2019-04-06 17:10:54 -0300

    feat(libcontainer): add contains_type to namespace_list
    Checks if a given type is on the namespace list

--- src/libcontainer/linux/namespace/list.rs
+++ src/libcontainer/linux/namespace/list.rs
@@ -1,4 +1,5 @@
 use super::Namespace;
+use super::NamespaceType;
 
 #[derive(Debug)]
 pub struct NamespaceList {
@@ -16,8 +17,14 @@ impl NamespaceList {
         &self.list
     }
 
-    pub fn insert(&mut self, other: Namespace) {
-        self.list.push(other);
+    pub fn insert(&mut self, namespace: Namespace) {
+        self.list.push(namespace);
+    }
+
+    pub fn contains_type(&self, ns_type: NamespaceType) -> bool {
+        self.list.iter().any(|ns|
+             ns.r#type == ns_type
+        )
     }
 }
 
@@ -27,6 +34,12 @@ mod tests {
 
     use crate::libcontainer::linux::namespace::Namespace;
     use crate::libcontainer::linux::namespace::NamespaceType;
+
+    #[test]
+    fn namespace_list_as_vec_returns_a_vector() {
+        let namespaces = NamespaceList::empty();
+        assert_eq!(namespaces.as_vec().len(), 0);
+    }
 
     #[test]
     fn namespace_list_empty_returns_an_empty_list() {
@@ -42,5 +55,18 @@ mod tests {
         namespaces.insert(namespace);
 
         assert_eq!(namespaces.list.len(), 1, "expect {:?} to be one element", &namespaces.list);
+    }
+
+    #[test]
+    fn namespace_list_contains_returns_false_if_ns_type_is_not_present() {
+        let namespaces = NamespaceList::empty();
+        assert_eq!(namespaces.contains_type(NamespaceType::UTS), false);
+    }
+
+    #[test]
+    fn namespace_list_contains_returns_true_if_ns_type_is_present() {
+        let mut namespaces = NamespaceList::empty();
+        namespaces.insert(Namespace::new(NamespaceType::UTS, None));
+        assert_eq!(namespaces.contains_type(NamespaceType::UTS), true);
     }
 }
