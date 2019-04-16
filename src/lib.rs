078ee3b0ccb6e1c591084635bd0933e757df12cc
Bernardo Lins <bernardolins28@gmail.com>
2019-04-16 08:49:03 -0300

    refactor: remove old container module

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ extern crate nix;
 
 pub mod bundle;
 pub mod config;
-pub mod container;
 pub mod error;
 
 pub mod libcontainer;
--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,15 @@ extern crate clap;
 extern crate cr7;
 
 use clap::App;
-use cr7::container;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    /*
-     * run command
-     */
     if let Some(matches) = matches.subcommand_matches("run") {
         let current_dir = str_current_dir();
         let container_id = matches.value_of("container-id").unwrap();
         let bundle_path = matches.value_of("bundle").unwrap_or(&current_dir);
-
-        match container::operations::run(container_id, bundle_path) {
-            Err(err) => {
-                println!("{}", err);
-                std::process::exit(1);
-            }
-            _ => ()
-        };
-    }
-
-    /*
-     * state command
-     */
-    if let Some(matches) = matches.subcommand_matches("state") {
-        let container_id = matches.value_of("container-id").unwrap();
-
-        match container::operations::state(container_id) {
-            Ok(state_json) => println!("{}", state_json),
-            Err(err) => println!("{}", err),
-        };
     }
 }
 
