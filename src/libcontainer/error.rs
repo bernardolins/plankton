4d44578155b46680cfb618b4cc18c6aafe6eea9c
Bernardo Lins <bernardolins28@gmail.com>
2019-04-19 19:50:17 -0300

    feat(error): implement libcontainer error from config error
    Implements From<ConfigError> for libcontainer Error

--- src/libcontainer/error.rs
+++ src/libcontainer/error.rs
@@ -1,6 +1,7 @@
 use crate::libcontainer::linux::mount::Error as MountError;
 use crate::libcontainer::linux::namespace::Error as NamespaceError;
 use crate::libcontainer::linux::environment::Error as EnvironmentError;
+use crate::libcontainer::config::Error as ConfigError;
 
 #[derive(Debug, PartialEq)]
 pub struct Error {
@@ -13,6 +14,13 @@ enum ErrorKind {
     MOUNT,
     ENVIRONMENT,
     NAMESPACE,
+    CONFIG,
+}
+
+impl std::error::Error for Error {
+    fn description(&self) -> &str {
+        &self.message
+    }
 }
 
 impl std::fmt::Display for Error {
@@ -27,6 +35,7 @@ impl std::fmt::Display for ErrorKind {
             ErrorKind::MOUNT => "mount error",
             ErrorKind::ENVIRONMENT => "environment error",
             ErrorKind::NAMESPACE => "namespace error",
+            ErrorKind::CONFIG => "config error",
 
         };
         write!(f, "{}", message)
@@ -55,6 +64,15 @@ impl From<EnvironmentError> for Error {
     fn from(err: EnvironmentError) -> Error {
         Error {
             kind: ErrorKind::ENVIRONMENT,
+            message: format!("{}", err),
+        }
+    }
+}
+
+impl From<ConfigError> for Error {
+    fn from(err: ConfigError) -> Error {
+        Error {
+            kind: ErrorKind::CONFIG,
             message: format!("{}", err),
         }
     }
