da525d442a777714b634e4fed6a5e28e02b6a7cb
Bernardo Lins <bernardolins28@gmail.com>
2019-04-19 20:02:09 -0300

    feat(environment): convert config working dir to environment working dir
    Reads the cwd option on the config file and set it on the environment

--- src/libcontainer/linux/environment/conv.rs
+++ src/libcontainer/linux/environment/conv.rs
@@ -11,8 +11,10 @@ impl TryFrom<Config> for Environment {
     fn try_from(config: Config) -> Result<Self, Self::Error> {
         let argv = config.process().args();
         let rootfs = config.root().path();
+        let mut environment = Environment::new(&argv[..], rootfs);
 
-        let environment = Environment::new(&argv[..], rootfs);
+        let working_dir = config.process().cwd();
+        environment.set_working_dir(working_dir)?;
 
         Ok(environment)
     }
