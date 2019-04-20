2d224b1ff4977329340c533b2ea62b1eb0315a2a
Bernardo Lins <bernardolins28@gmail.com>
2019-04-20 00:55:16 -0300

    feat(config): add linux specific config
    Support namespace list of linux config

---
+++ src/libcontainer/config/linux.rs
@@ -0,0 +1,27 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Linux {
+    pub namespaces: Vec<Namespace>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Namespace {
+    r#type: String,
+    path: Option<String>,
+}
+
+impl Namespace {
+    pub fn ns_type(&self) -> &str {
+        &self.r#type
+    }
+
+    pub fn path(&self) -> Option<&str> {
+        if let Some(path) = &self.path {
+            return Some(&path)
+        }
+        None
+    }
+}
--- src/libcontainer/config/mod.rs
+++ src/libcontainer/config/mod.rs
@@ -2,6 +2,9 @@ pub mod root;
 pub mod process;
 pub mod error;
 
+#[cfg(target_os = "linux")]
+pub mod linux;
+
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
@@ -10,13 +13,18 @@ use serde::{Serialize, Deserialize};
 
 pub use self::error::Error;
 
+pub use self::linux::Namespace;
+
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
     oci_version: String,
     hostname: Option<String>,
     root: root::Root,
-    process: process::Process
+    process: process::Process,
+
+    #[cfg(target_os = "linux")]
+    linux: linux::Linux,
 }
 
 impl Config {
@@ -24,6 +32,12 @@ impl Config {
     pub fn oci_version(&self) -> &str { &self.oci_version }
     pub fn root(&self) -> &root::Root { &self.root }
     pub fn process(&self) -> &process::Process { &self.process }
+
+
+    #[cfg(target_os = "linux")]
+    pub fn namespaces(&self) -> &Vec<Namespace> {
+        &self.linux.namespaces
+    }
 
     pub fn load(config_path: &PathBuf) -> Result<Config, Error> {
         let file = File::open(&config_path)?;
