8234cbeaa2932b5acb9efde834520c15426b0f88
Bernardo Lins <bernardolins28@gmail.com>
2019-04-21 10:38:39 -0300

    feat(namespace): create a namespace type from &str

--- src/libcontainer/linux/namespace/type.rs
+++ src/libcontainer/linux/namespace/type.rs
@@ -8,3 +8,43 @@ pub enum NamespaceType {
     CGROUP,
     NETWORK,
 }
+
+impl NamespaceType {
+    fn from_str(original: &str) -> Option<NamespaceType> {
+        match original {
+            "pid" => Some(NamespaceType::PID),
+            "uts" => Some(NamespaceType::UTS),
+            "ipc" => Some(NamespaceType::IPC),
+            "user" => Some(NamespaceType::USER),
+            "mount" => Some(NamespaceType::MOUNT),
+            "cgroup" => Some(NamespaceType::CGROUP),
+            "network" => Some(NamespaceType::NETWORK),
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn namespace_type_from_str() {
+        let table = vec![
+            ("pid", Some(NamespaceType::PID)),
+            ("uts", Some(NamespaceType::UTS)),
+            ("ipc", Some(NamespaceType::IPC)),
+            ("user", Some(NamespaceType::USER)),
+            ("mount", Some(NamespaceType::MOUNT)),
+            ("cgroup", Some(NamespaceType::CGROUP)),
+            ("network", Some(NamespaceType::NETWORK)),
+            ("invalid", None),
+        ];
+
+        for (original, result) in table {
+            assert_eq!(NamespaceType::from_str(original), result);
+        }
+    }
+}
+
+
