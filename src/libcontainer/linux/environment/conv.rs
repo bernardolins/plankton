54416538ced42292d5648a986b773f9270a1ad1f
Bernardo Lins <bernardolins28@gmail.com>
2019-04-21 11:09:12 -0300

    feat(environment): set namespaces from config on environment

--- src/libcontainer/linux/environment/conv.rs
+++ src/libcontainer/linux/environment/conv.rs
@@ -3,6 +3,8 @@ use std::convert::TryFrom;
 use super::Environment;
 
 use crate::libcontainer::Config;
+use crate::libcontainer::Namespace;
+use crate::libcontainer::NamespaceType;
 use crate::libcontainer::error::Error;
 
 impl TryFrom<Config> for Environment {
@@ -15,6 +17,18 @@ impl TryFrom<Config> for Environment {
 
         let working_dir = config.process().cwd();
         environment.set_working_dir(working_dir)?;
+
+        for namespace_config in config.namespaces() {
+            let str_type = namespace_config.ns_type();
+            let path = match namespace_config.path() {
+                Some(str_path) => Some(str_path.to_string()),
+                None => None,
+            };
+
+            let ns_type = NamespaceType::from_str(str_type)?;
+            let namespace = Namespace::new(ns_type, path);
+            environment.set_namespace(namespace)?;
+        }
 
         if let Some(hostname) = config.hostname() {
             environment.set_hostname(hostname)?;
