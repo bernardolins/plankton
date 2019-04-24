cdda4845baa8f853012eb51bb60dc477b5b78428
Bernardo Lins <bernardolins28@gmail.com>
2019-04-24 07:29:51 -0300

    feat(config): add optional environment list to process config

--- src/libcontainer/config/process.rs
+++ src/libcontainer/config/process.rs
@@ -4,10 +4,12 @@ use serde::{Serialize, Deserialize};
 #[serde(rename_all = "camelCase")]
 pub struct Process {
     args: Vec<String>,
+    env: Option<Vec<String>>,
     cwd: String,
 }
 
 impl Process {
     pub fn args(&self) -> &Vec<String> { &self.args }
+    pub fn env(&self) -> &Option<Vec<String>> { &self.env }
     pub fn cwd(&self) -> &str { &self.cwd }
 }
