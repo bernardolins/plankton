64080c1703cf3474e815e790eb9044d203637ec3
Bernardo Lins <bernardolins28@gmail.com>
2019-04-25 08:06:06 -0300

    feat(config): add rlimits to config

--- src/libcontainer/config/process/mod.rs
+++ src/libcontainer/config/process/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(target_os = "linux")]
+pub mod rlimit;
+
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -6,10 +9,16 @@ pub struct Process {
     args: Vec<String>,
     env: Option<Vec<String>>,
     cwd: String,
+
+    #[cfg(target_os = "linux")]
+    rlimits: Vec<rlimit::Rlimit>,
 }
 
 impl Process {
     pub fn args(&self) -> &Vec<String> { &self.args }
     pub fn env(&self) -> &Option<Vec<String>> { &self.env }
     pub fn cwd(&self) -> &str { &self.cwd }
+
+    #[cfg(target_os = "linux")]
+    pub fn rlimits(&self) -> &Vec<rlimit::Rlimit> { &self.rlimits }
 }
