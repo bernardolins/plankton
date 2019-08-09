9c9fa8e4e062e298c2f58963350ccd95b00a259e
Bernardo Lins <bernardolins28@gmail.com>
2019-08-08 23:18:26 -0300

    fix: bring deleted exports back

--- src/libcontainer/mod.rs
+++ src/libcontainer/mod.rs
@@ -1,2 +1,7 @@
 #[cfg(target_os = "linux")] pub mod linux;
 #[cfg(target_os = "linux")] pub use self::linux::environment::Environment;
+#[cfg(target_os = "linux")] pub use self::linux::namespace::Namespace;
+#[cfg(target_os = "linux")] pub use self::linux::namespace::NamespaceList;
+#[cfg(target_os = "linux")] pub use self::linux::namespace::NamespaceType;
+#[cfg(target_os = "linux")] pub use self::linux::mount::MountPoint;
+#[cfg(target_os = "linux")] pub use self::linux::rlimit::Rlimit;
