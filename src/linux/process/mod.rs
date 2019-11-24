39a594d79a38d322d31d48bec430794b2aa3dec1
Bernardo Lins <bernardolins28@gmail.com>
2019-11-24 11:49:18 -0300

    feat(linux): add env_vars to process

--- src/linux/process/mod.rs
+++ src/linux/process/mod.rs
@@ -2,6 +2,7 @@ use crate::Error;
 use crate::spec::ProcessSpec;
 use failure::ResultExt;
 use std::ffi::CString;
+use std::ffi::OsString;
 use std::path::PathBuf;
 use std::process::Command;
 use std::os::unix::process::CommandExt;
@@ -11,19 +12,27 @@ pub struct Process {
     args: Option<Vec<CString>>,
     attach_terminal: bool,
     working_dir: PathBuf,
+    env_vars: Option<Vec<(OsString, OsString)>>,
 }
 
 impl Process {
     pub fn from_spec<P: ProcessSpec>(proc_spec: &P) -> Result<Process, Error> {
         let attach_terminal = !(proc_spec.get_terminal().is_none()) && *proc_spec.get_terminal().unwrap();
+
         let spec_args = proc_spec.get_args_clone();
         let args = into_cstring_vec(spec_args)?;
+
         let working_dir = proc_spec.get_cwd_clone();
         check_working_dir(&working_dir)?;
+
+        let spec_envs = proc_spec.get_env();
+        let env_vars = parse_envs(spec_envs)?;
+
         Ok(Process{
             attach_terminal,
             args,
             working_dir,
+            env_vars,
         })
     }
 }
@@ -48,6 +57,27 @@ fn check_working_dir(working_dir: &PathBuf) -> Result<(), Error> {
     Ok(())
 }
 
+fn parse_envs(envs: Option<&Vec<String>>) -> Result<Option<Vec<(OsString, OsString)>>, Error> {
+    if envs.is_none() {
+        return Ok(None)
+    }
+    let mut env_vars: Vec<(OsString, OsString)> = Vec::new();
+    for str_env in envs.unwrap() {
+        let mut splitted_env: Vec<&str> = str_env.split("=").collect();
+        let error_message = "environment variable must have 'KEY=VALUE' format";
+        if splitted_env.len() != 2 {
+            Err(Error::from(error_message.to_string())).context(str_env.to_string())?
+        }
+        let k = OsString::from(splitted_env.remove(0));
+        let v = OsString::from(splitted_env.remove(0));
+        if k.is_empty() {
+            Err(Error::from(error_message.to_string())).context(str_env.to_string())?
+        }
+        env_vars.push((k, v));
+    }
+    Ok(Some(env_vars))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,6 +85,7 @@ mod tests {
     use crate::spec::ProcessSpec;
     use crate::spec::ConsoleSizeSpec;
     use std::ffi::CString;
+    use std::ffi::OsString;
     use std::path::PathBuf;
 
     #[derive(Debug)]
@@ -96,7 +127,7 @@ mod tests {
         FakeProcess {
             fake_terminal: None,
             fake_console_size: None,
-            fake_cwd: PathBuf::from(""),
+            fake_cwd: PathBuf::from("/"),
             fake_env: None,
             fake_args: None,
         }
@@ -104,6 +135,16 @@ mod tests {
 
     fn to_cstring(s: &str) -> CString {
         CString::new(s).unwrap()
+    }
+
+    fn to_osstring_vec(items: Vec<(&str, &str)>) -> Vec<(OsString, OsString)> {
+        let mut result: Vec<(OsString, OsString)> = Vec::new();
+        for (k, v) in items {
+            let os_k = OsString::from(k);
+            let os_v = OsString::from(v);
+            result.push((os_k, os_v));
+        }
+        result
     }
 
     #[test]
@@ -151,6 +192,30 @@ mod tests {
             if result.is_ok() {
                 let proc = result.unwrap();
                 assert_eq!(proc.working_dir, expected, "expected process args to be {:?} but got {:?}", expected, proc.working_dir);
+            }
+        }
+    }
+
+    #[test]
+    fn from_spec_env_vars() {
+        let table = vec![
+            (None, None, true),
+            (Some(vec![]), Some(vec![]), true),
+            (Some(vec!["=TERM=xterm".to_string()]), None, false),
+            (Some(vec!["TERM=xterm".to_string(), "=PATH=/".to_string()]), None, false),
+            (Some(vec!["=xterm".to_string()]), None, false),
+            (Some(vec!["PATH=".to_string()]), Some(to_osstring_vec(vec![("PATH", "")])), true),
+            (Some(vec!["=".to_string()]), None, false),
+            (Some(vec!["PATH=/bin:/usr/bin".to_string()]), Some(to_osstring_vec(vec![("PATH", "/bin:/usr/bin")])), true),
+        ];
+        for (spec_value, expected, is_ok) in table {
+            let mut spec = empty_spec();
+            spec.fake_env = spec_value;
+            let result = Process::from_spec(&spec);
+            assert_eq!(result.is_ok(), is_ok, "expected result.is_ok to be {}, but result was {:?}", is_ok, &result);
+            if result.is_ok() {
+                let proc = result.unwrap();
+                assert_eq!(proc.env_vars, expected, "expected process args to be {:?} but got {:?}", expected, proc.env_vars);
             }
         }
     }
